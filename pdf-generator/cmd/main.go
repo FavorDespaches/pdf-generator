b2e570e6ae0c3f9ae8574fcec74e70cb13cec3c0
Gabriel Faglioni Mendes <umfavor4@gmail.com>
2024-04-24 13:43:06 -0300

    Adição do template de Cartas simples

--- pdf-generator/cmd/main.go
+++ pdf-generator/cmd/main.go
@@ -17,18 +17,6 @@ func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse
 	case "GET":
 		return handlers.HandleUnsupportedMethod()
 	case "POST":
-		//handlers.SolicitarEtiqueta(req) //handlers.SolicitarEtiqueta(req)
-
-		/*
-			dirPath := "/opt/bin/images" // Replace with the path of your directory
-			contents := printAndReturnContents(dirPath)
-			successBody := handlers.SuccessResponse{
-				IdPlp:       0,
-				Etiquetas:   []string{""},
-				LabelBase64: contents,
-			}
-		*/
-
 		return handlers.SolicitarEtiqueta(req)
 	case "PUT":
 		return handlers.HandleUnsupportedMethod()
